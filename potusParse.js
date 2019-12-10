8821696a2565282dc0225dcd1685814bb4e4f951
ruicorte <ruicorte@gmail.com>
2019-12-10 23:04:30 +0000

    code cleanup

--- potusParse.js
+++ potusParse.js
@@ -15,21 +15,3 @@ const potusParse = function(url) {
 };
 
 module.exports = potusParse;
-/* 
-const rp = require('request-promise');
-const $ = require('request');
-
-const potusParse = function(url) {
-    return rp(url)
-        .then(function(html) {
-            return {
-                name: $('.firstHeading', html).text(),
-                birth: $('.bday', html).text(),
-            }
-        })
-        .catch(function(err) {
-            console.log(err)
-        });
-}
-
-module exports = potusParse; */
