aecdaf02789151327c9de8a29c94fe1abfd5f8ac
ruicorte <ruicorte@gmail.com>
2019-12-10 23:03:18 +0000

    code cleanup

--- potusScrapper.js
+++ potusScrapper.js
@@ -5,15 +5,11 @@ const url = 'https://en.wikipedia.org/wiki/List_of_Presidents_of_the_United_Stat
 
 rp(url)
     .then(function(html) {
-        // console.log($('big > a', html).length);
-        // console.log($('big > a', html));
         const wikiUrls = [];
-
         const presidents = $('big > a', html);
         for (let i = 0; i < presidents.length; i++) {
             wikiUrls.push(presidents[i].attribs.href);
         }
-
         return Promise.all(
                 wikiUrls.map((function(url) {
                     return potusParse('https://en.wikipedia.org' + url);
@@ -29,31 +25,3 @@ rp(url)
     .catch(function(err) {
         console.log(err);
     });
-
-/*
-    const rp = require('request-promise');
-const $ = require('cheerio');
-const potusParse = require('./potusParse');
-const url = 'https://en.wikipedia.org/wiki/List_of_Presidents_of_the_United_States';
-
-rp(url)
-  .then(function(html) {
-    //success!
-    const wikiUrls = [];
-    for (let i = 0; i < 45; i++) {
-      wikiUrls.push($('big > a', html)[i].attribs.href);
-    }
-    return Promise.all(
-      wikiUrls.map(function(url) {
-        return potusParse('https://en.wikipedia.org' + url);
-      })
-    );
-  })
-  .then(function(presidents) {
-    console.log(presidents);
-  })
-  .catch(function(err) {
-    //handle error
-    console.log(err);
-  });
-    */
